4085f408c053e98c0e082c89cb0213605766f12a
Keleborn <22352763+Celandriel@users.noreply.github.com>
2025-11-17 13:22:27 -0800

    Fix.

--- src/strategy/values/FishValues.h
+++ src/strategy/values/FishValues.h
@@ -29,7 +29,7 @@ public:
 class FishingSpotValue : public ManualSetValue<WorldPosition>
 {
 public:
-    FishingSpotValue(PlayerbotAI* botAI, WorldPosition auto& const  pos = WorldPosition(), std::string const name = "fishing spot")
+    FishingSpotValue(PlayerbotAI* botAI, WorldPosition const& pos = WorldPosition(), std::string const name = "fishing spot")
      : ManualSetValue<WorldPosition>(botAI, pos, name) {}
 
     void Set(WorldPosition val) override
