9abafe8e52b29fba1aa9bfabd4d8dede39b3d7ae
Keleborn <22352763+Celandriel@users.noreply.github.com>
2025-12-27 07:08:43 -0800

    Update sql id

--- data/sql/playerbots/updates/2025_10_13_ai_playerbot fishing text.sql
+++ data/sql/playerbots/updates/2025_10_13_ai_playerbot fishing text.sql
@@ -2,7 +2,7 @@ DELETE FROM ai_playerbot_texts WHERE name IN ('no_fishing_pole_error');
 DELETE FROM ai_playerbot_texts_chance WHERE name IN ('no_fishing_pole_error');
 
 INSERT INTO ai_playerbot_texts (id, name, text, say_type, reply_type, text_loc1, text_loc2, text_loc3, text_loc4, text_loc5, text_loc6, text_loc7, text_loc8) VALUES
-(1717, 'no_fishing_pole_error',   "I don't have a Fishing Pole",   0, 0, 
+(1736, 'no_fishing_pole_error',   "I don't have a Fishing Pole",   0, 0, 
 "낚싯대가 없습니다", 
 "Je n’ai pas de canne à pêche", 
 "Ich habe keine Angelrute", 
